758ba978ef2de063b6d9da5138654e264d0a2c2e
Arslaan Ahmad Malik <arslaanmalik@yahoo.com>
2021-12-27 22:13:10 +0500

    Update master.sh

--- master.sh
+++ master.sh
@@ -41,8 +41,7 @@ sudo sysctl --system
 
 echo "Installing Docker..."
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo
-https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io
 sudo mkdir /etc/docker
 sudo mkdir -p /etc/systemd/system/docker.service.d
