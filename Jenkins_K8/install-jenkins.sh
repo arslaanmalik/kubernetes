0a71a94ff12542985cda7d3eb6735ac973102297
Arslaan Ahmad Malik <arslaanmalik@yahoo.com>
2022-02-20 00:24:15 +0500

    jenkins kubernetes setup

---
+++ Jenkins_K8/install-jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Create the PV directory first"
+
+echo "Creating Namespace Jenkins "
+kubectl create namespace jenkins
+
+echo "Create Persistant Volume"
+kubectl apply -f /Jenkins_K8_Yaml/pv.yml
+
+echo "Setup Kubernetes Jenkins"
+kubectl apply -f $workspace/jenkins-deployment.yml -n jenkins
