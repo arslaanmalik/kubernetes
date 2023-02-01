0b7d9676ec92429404f618bc164ecc9fde932672
Arslaan Ahmad Malik <arslaanmalik@yahoo.com>
2023-02-02 02:58:04 +0500

    Master Slave Builds

--- Jenkins_K8/install-jenkins.sh
+++ Jenkins_K8/install-jenkins.sh
@@ -8,6 +8,10 @@ kubectl create namespace jenkins
 echo "Create Persistant Volume"
 kubectl apply -f /Jenkins_K8_Yaml/pv.yml
 
+echo "Creating Master and Slave Images"
+docker build -t kubernetes/Jenkins_K8/Jenkins_DockerFiles/Master docker-registry:5000/jenkins:master .
+docker build -t kubernetes/Jenkins_K8/Jenkins_DockerFiles/Slave docker-registry:5000/jenkins:slave .
+
 echo "Setup Kubernetes Jenkins"
 kubectl apply -f $workspace/jenkins-deployment.yml -n jenkins
 echo "Creating Secret from Htpasswd"
