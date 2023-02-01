e0cc5c0bfc8cb7ac80ffd970ad87500bdafad549
Arslaan Ahmad Malik <arslaanmalik@yahoo.com>
2023-02-02 02:55:09 +0500

    added secret in scripts for registry

--- Jenkins_K8/install-jenkins.sh
+++ Jenkins_K8/install-jenkins.sh
@@ -10,4 +10,7 @@ kubectl apply -f /Jenkins_K8_Yaml/pv.yml
 
 echo "Setup Kubernetes Jenkins"
 kubectl apply -f $workspace/jenkins-deployment.yml -n jenkins
+echo "Creating Secret from Htpasswd"
+kubectl create secret docker-registry reg-cred-secret --docker-server=$REGISTRY_NAME:5000 --docker-username=myuser --docker-password=mypasswd
+
 
