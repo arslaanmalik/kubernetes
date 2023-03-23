206c724e741504f8789ff12d1d5eff84724103ee
Arslaan Ahmad Malik <arslaanmalik@yahoo.com>
2023-03-23 16:43:58 +0500

    fixed containerd 1.26.2 k8

--- K8-Setup-containerd/master.sh
+++ K8-Setup-containerd/master.sh
@@ -3,6 +3,7 @@
 echo "Updating Yum...."
 sudo yum -y update
 
+
 echo "Installing Kubelet, Kubeadm and Kubectl......"
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
@@ -10,10 +11,11 @@ name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
 enabled=1
 gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 exclude=kubelet kubeadm kubectl
 EOF
+
+#gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 
 #sudo yum -y install epel-release vim git curl wget kubelet kubeadm kubectl --disableexcludes=kubernetes
 sudo yum -y install epel-release vim git curl wget kubelet-1.26.2 kubeadm-1.26.2-0 kubectl-1.26.2 --disableexcludes=kubernetes
