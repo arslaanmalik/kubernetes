3b7086db7a626cb375fe894b19812a89ae83efe3
Arslaan Ahmad Malik <arslaanmalik@yahoo.com>
2023-03-23 18:54:01 +0500

    worker fixes

--- K8-Setup-containerd/worker.sh
+++ K8-Setup-containerd/worker.sh
@@ -37,6 +37,8 @@ net.ipv4.ip_forward = 1
 EOF
 sudo sysctl --system
 
+sleep 30
+
 echo "Installing Containerd..."
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
