258bf6373ad09fa4484fab5865107fbf543adffb
Arslaan Ahmad Malik <arslaanmalik@yahoo.com>
2023-08-14 03:37:18 +0500

    updated ingress and metrics server

---
+++ Metrics-Server/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Installing K8 Ingress"
+kubectl apply -f metrics-server.yaml
+
---
+++ nginx-ingress/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "What Port do you want to set the K8 ingress port to?"
+# $http=
+# $https=
+
+
+echo "Installing K8 Ingress"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/baremetal/deploy.yaml
+
+echo "Get Services"
+kubectl get svc -n ingress-nginx
+echo "Create Patching Yaml for Nodeport Dashboard"
+cat << EOF > ingress_svc-nodeport_patch.yaml
+spec:
+  ports:
+  - appProtocol: http
+    name: http
+    nodePort: 30008
+    port: 80
+    protocol: TCP
+    targetPort: http
+  - appProtocol: https
+    name: https
+    nodePort: 30009
+    port: 443
+    protocol: TCP
+    targetPort: https
+EOF
+
+echo "Apply Patch"
+kubectl -n ingress-nginx patch svc ingress-nginx-controller --patch "$(cat ingress_svc-nodeport_patch.yaml)"
